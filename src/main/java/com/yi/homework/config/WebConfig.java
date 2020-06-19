eff73ea64624d070f2159aa12ba34dcb07e48b46
yipple <1275432232@qq.com>
2020-06-19 23:06:01 +0800

    加入对前端路由history模式的支持

--- src/main/java/com/yi/homework/config/WebConfig.java
+++ src/main/java/com/yi/homework/config/WebConfig.java
@@ -1,6 +1,11 @@
 package com.yi.homework.config;
 
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -15,6 +20,20 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  */
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    /**
+     * 支持前端路由的 history 模式
+     */
+    @Bean
+    public WebServerFactoryCustomizer webServerFactoryCustomizer() {
+        return new WebServerFactoryCustomizer<ConfigurableServletWebServerFactory>() {
+            @Override
+            public void customize(ConfigurableServletWebServerFactory factory) {
+                ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/index.html");
+                factory.addErrorPages(error404Page);
+            }
+        };
+    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
